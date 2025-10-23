e1e72d32879fb714cb6e41057f3cdb9dd4ee1466
yuanjie.dong <dongyuanjie@able-elec.com>
2025-10-23 18:41:56 +0800

    feat: commitlint

---
+++ scripts/commit-lint.ts
@@ -0,0 +1,41 @@
+import { readFileSync } from 'node:fs';
+import { logger } from 'rslog';
+
+const commitRE = /^(revert: )?(fix|feat|docs|perf|test|types|style|build|chore|ci|release|refactor|breaking change)(\(.+\))?: .{1,50}/;
+const mergeRE = /Merge /;
+
+function commitLint(gitParams: string) {
+	const commitMsg = readFileSync(gitParams, 'utf-8').trim();
+
+	if (!commitRE.test(commitMsg) && !mergeRE.test(commitMsg)) {
+		logger.error(`invalid commit message: "${commitMsg}".
+
+Proper commit message format is required for automated changelog generation.
+
+Examples: 
+
+- fix(button): incorrect style
+- feat(button): incorrect style
+- docs(button): fix typo
+
+Allowed Types:
+
+- fix
+- feat
+- docs
+- perf
+- test
+- types
+- build
+- chore
+- ci
+- release
+- refactor
+- breaking change
+- Merge branch 'button' into 'main'
+`);
+		process.exit(1);
+	}
+}
+
+commitLint(process.argv[2]);
