96f8b5a923f903ec25c358c241ca63e9bca4bef6
sxy15 <sxy_19921115@163.com>
2025-11-10 11:19:25 +0800

    feat(ui): test

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+export default defineConfig({
+    plugins: [vue(), vueJsx()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: [],
+        coverage: {
+            provider: 'v8',
+            reporter: ['html'],
+        },
+    },
+});
